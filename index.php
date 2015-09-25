d4d86170afeeb9d340a8ae9443a1ca31d1efcd20
Benjamin <benji@Benji.(none)>
2015-09-25 02:04:38 +0200

    Filter access by name

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 if(isset($_GET['n']))
 echo "Salut ".htmlentities($_GET['n']);
+else exit();
+
+$name = array("Mélanie","Nicolas","Emilie");
+if(in_array($_GET['n'],$name))
+echo "<br><br>Access Granted";
 ?>
